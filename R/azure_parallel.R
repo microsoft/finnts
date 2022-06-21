be068af5ae9538deaeb6ce5da9c352c878228db3
Mike Tokic <mitokic@microsoft.com>
2022-06-21 07:40:10 -0700

    update messaging around spark submissions

--- R/azure_parallel.R
+++ R/azure_parallel.R
@@ -66,7 +66,7 @@ get_fcast_parallel_azure_spark <- function(combo_list,
     stop("Ensure that you are connected to a spark cluster using an object called 'sc'")
   }
   
-  cli::cli_h2("Connecting to Spark Cluster")
+  cli::cli_h2("Submitting Tasks to Spark Cluster")
   
   sparklyr::registerDoSpark(sc, parallelism = length(combo_list))
   
