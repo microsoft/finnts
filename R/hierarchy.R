85d4fc7637031330f7bc74f156518d1303a6f417
Mike Tokic <mitokic@microsoft.com>
2022-12-02 18:37:04 -0800

    debug

--- R/hierarchy.R
+++ R/hierarchy.R
@@ -114,7 +114,8 @@ prep_hierarchical_data <- function(input_data,
 adjust_df <- function(input_data,
                       return_type = "df") {
   if (return_type == "sdf" & inherits(input_data, c("tbl", "tbl_df", "data.frame"))) {
-    sparklyr::sdf_copy_to(sc, input_data, overwrite = TRUE)
+    input_data <- sparklyr::sdf_copy_to(sc, input_data, overwrite = TRUE)
+    return(input_data)
   } else if (return_type == "sdf" & inherits(input_data, "tbl_spark")) {
     input_data
   } else {
