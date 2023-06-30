85e628e4e3ed4e576881f0eccc973f6d28cfbcb3
Mike Tokic <mitokic@microsoft.com>
2023-06-30 15:38:43 -0700

    add message when tuning fails in train_models

--- R/train_models.R
+++ R/train_models.R
@@ -328,7 +328,6 @@ train_models <- function(run_info,
         .noexport = NULL
       ) %op%
         {
-
           # run input values
           param_combo <- x %>%
             dplyr::pull(Hyperparameter_ID)
@@ -443,6 +442,29 @@ train_models <- function(run_info,
 
       par_end(inner_cl)
 
+      # check if tuning failed
+      if (is.null(initial_tune_tbl)) {
+        if (x == "All-Data") {
+          combo_name <- "Global-Model"
+        } else {
+          combo_name <- model_recipe_tbl %>%
+            dplyr::slice(1) %>%
+            dplyr::select(Data) %>%
+            tidyr::unnest(Data) %>%
+            dplyr::select(Combo) %>%
+            dplyr::pull(Combo) %>%
+            unique()
+        }
+
+        stop(paste0(
+          "All models failed during hyperparameter tuning process for time series combo: '",
+          combo_name, "'"
+        ),
+        call. = FALSE
+        )
+      }
+
+      # select best hyperparamters
       best_param <- initial_tune_tbl %>%
         tidyr::unnest(Prediction) %>%
         dplyr::mutate(SE = (Target - Forecast)^2) %>%
