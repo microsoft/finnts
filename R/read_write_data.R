6e493fb26e540d995937ca75588acfffd891cbfc
Mike Tokic <mitokic@microsoft.com>
2024-08-02 09:14:37 -0700

    attatch package

--- R/read_write_data.R
+++ R/read_write_data.R
@@ -417,7 +417,7 @@ write_data_type <- function(x,
     rds = saveRDS(x, path),
     parquet = arrow::write_parquet(x, path),
     csv = vroom::vroom_write(x, path, delim = ",", progress = FALSE),
-    log = write.csv(x, path, row.names = FALSE),
+    log = base::write.csv(x, path, row.names = FALSE),
     qs = qs::qsave(x, path)
   )
 }
