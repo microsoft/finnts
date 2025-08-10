0f867180761da1005681ed8a9d4d4617ff55d092
mitokic <mitokic@microsoft.com>
2025-08-10 14:29:56 -0700

    debug

--- R/train_models.R
+++ R/train_models.R
@@ -520,9 +520,7 @@ train_models <- function(run_info,
                 min_n         = 1,
                 mtry          = 0.8,   # maps to colsample_bynode
                 sample_size   = 1,     # <- maps to XGBoost 'subsample' in (0,1]; 1 is fine
-                loss_reduction= 0,
-                stop_iter     = 0, 
-                early_stop    = 2
+                loss_reduction= 0
                 # NOTE: we DON'T touch your custom args (lag_periods, forecast_horizon, external_regressors)
               )
           )
