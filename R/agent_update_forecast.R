636791a464931006570739521bdcce0bb5dbe042
mitokic <mitokic@microsoft.com>
2025-08-20 05:31:32 -0700

    update model_update

--- R/agent_update_forecast.R
+++ R/agent_update_forecast.R
@@ -135,7 +135,7 @@ update_fcst_agent_workflow <- function(agent_info,
       fn = "update_local_models",
       `next` = "analyze_results",
       retry_mode = "plain",
-      max_retry = 3,
+      max_retry = 0,
       args = list(
         agent_info = agent_info,
         previous_best_run_tbl = "{results$initial_checks}",
